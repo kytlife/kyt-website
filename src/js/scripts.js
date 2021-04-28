04278b6d10459fcfe28f2064b8c22e13a6d38377
Jason Villanueva <a@jsonvillanueva.com>
2021-04-27 17:23:21 -0700

    Added google form logic and jquery animation

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,50 +1,54 @@
-(function ($) {
-    "use strict"; // Start of use strict
+;(function ($) {
+  'use strict' // Start of use strict
 
-    // Smooth scrolling using anime.js
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').on('click', function () {
-        if (
-            location.pathname.replace(/^\//, "") ==
-            this.pathname.replace(/^\//, "") &&
-            location.hostname == this.hostname
-        ) {
-            var target = $(this.hash);
-            target = target.length ?
-                target :
-                $("[name=" + this.hash.slice(1) + "]");
-            if (target.length) {
-                anime({
-                    targets: 'html, body',
-                    scrollTop: target.offset().top - 72,
-                    duration: 1000,
-                    easing: 'easeInOutExpo'
-                });
-                return false;
-            }
-        }
-    });
+  // Smooth scrolling using anime.js
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').on('click', function () {
+    if (
+      location.pathname.replace(/^\//, '') ==
+        this.pathname.replace(/^\//, '') &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash)
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']')
+      if (target.length) {
+        anime({
+          targets: 'html, body',
+          scrollTop: target.offset().top - 72,
+          duration: 1000,
+          easing: 'easeInOutExpo',
+        })
+        return false
+      }
+    }
+  })
 
-    // Closes responsive menu when a scroll trigger link is clicked
-    $(".js-scroll-trigger").click(function () {
-        $(".navbar-collapse").collapse("hide");
-    });
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function () {
+    $('.navbar-collapse').collapse('hide')
+  })
 
-    // Activate scrollspy to add active class to navbar items on scroll
-    $("body").scrollspy({
-        target: "#mainNav",
-        offset: 100,
-    });
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 100,
+  })
+})(jQuery) // End of use strict
 
-    // Collapse Navbar
-    var navbarCollapse = function () {
-        if ($("#mainNav").offset().top > 100) {
-            $("#mainNav").addClass("navbar-shrink");
-        } else {
-            $("#mainNav").removeClass("navbar-shrink");
-        }
-    };
-    // Collapse now if page is not at top
-    navbarCollapse();
-    // Collapse the navbar when page is scrolled
-    $(window).scroll(navbarCollapse);
-})(jQuery); // End of use strict
+var submitted = false
+$('#gform').on('submit', function (e) {
+  $('#intouch').animate(
+    {
+      opacity: 'toggle',
+      fontSize: 'toggle',
+    },
+    500
+  )
+  $('#gform *').animate(
+    {
+      opacity: '0',
+      height: '0px',
+      width: '0px',
+    },
+    500
+  )
+})
